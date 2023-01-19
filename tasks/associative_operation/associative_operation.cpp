25bfc67e91630c5584bf75aa50ab658c716a708a
Yaroslav Starostin <yistarostin@gmail.com>
2023-01-19 18:15:31 +0300

    fix bug in assciative_operation

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -3,7 +3,7 @@ bool IsAssociative(const std::vector<std::vector<std::size_t>>& table) {
     for (std::size_t x = 0; x < table.size(); ++x) {
         for (std::size_t y = 0; y < table.size(); ++y) {
             for (std::size_t z = 0; z < table.size(); ++z) {
-                if (table[table[x][y]][z] != table[x][table[x][y]]) {
+                if (table[table[x][y]][z] != table[x][table[y][z]]) {
                     return false;
                 }
             }
