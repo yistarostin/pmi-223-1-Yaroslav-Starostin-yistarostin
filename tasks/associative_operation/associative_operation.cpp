43fb6c7a28485a66ee81773f15c5cbe99ad2083b
Yaroslav Starostin <yistarostin@gmail.com>
2023-01-19 17:58:43 +0300

    make associative_operation.cpp prettier

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -1,9 +1,9 @@
 #include "associative_operation.h"
 bool IsAssociative(const std::vector<std::vector<std::size_t>>& table) {
-    for(std::size_t x = 0; x < table.size(); ++x){
-        for(std::size_t y = 0; y < table.size(); ++y){
-            for(std::size_t z = 0; z < table.size(); ++z){
-                if (table[table[x][y]][z] != table[x][table[x][y]]){
+    for (std::size_t x = 0; x < table.size(); ++x) {
+        for (std::size_t y = 0; y < table.size(); ++y) {
+            for (std::size_t z = 0; z < table.size(); ++z) {
+                if (table[table[x][y]][z] != table[x][table[x][y]]) {
                     return false;
                 }
             }
