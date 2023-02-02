1e19b74cb0f0034d3466fea49b7f6e6be1b37cef
Yaroslav Starostin <yistarostin@gmail.com>
2023-02-02 20:05:52 +0300

    fix tests_checking.cpp problems (use reserve() and push_back when possible)

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -5,15 +5,16 @@
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    std::deque<std::string> papers_order;
+    std::deque<std::string_view> papers_order;
     std::for_each(student_actions.begin(), student_actions.end(), [&](const StudentAction& paper) {
         if (paper.side == Side::Bottom) {
-            papers_order.emplace_back(paper.name);
+            papers_order.push_back(paper.name);
         } else {
-            papers_order.emplace_front(paper.name);
+            papers_order.push_front(paper.name);
         }
     });
-    std::vector<std::string> queried_positions_owners{};
+    std::vector<std::string> queried_positions_owners;
+    queried_positions_owners.reserve(queries.size());
     std::for_each(queries.begin(), queries.end(),
                   [&](size_t position) { queried_positions_owners.emplace_back(papers_order[position - 1]); });
     return queried_positions_owners;
