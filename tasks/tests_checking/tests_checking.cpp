0ce5c0706de5e54cd819d6b5caf4cb6592682aa2
Yaroslav Starostin <yistarostin@gmail.com>
2023-02-02 15:06:27 +0300

    solve tests_checking

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,6 +1,20 @@
 #include "tests_checking.h"
 
+#include <algorithm>
+#include <deque>
+
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    return {};
+    std::deque<std::string> papers_order;
+    std::for_each(student_actions.begin(), student_actions.end(), [&](const StudentAction& paper) {
+        if (paper.side == Side::Bottom) {
+            papers_order.emplace_back(paper.name);
+        } else {
+            papers_order.emplace_front(paper.name);
+        }
+    });
+    std::vector<std::string> queried_positions_owners{};
+    std::for_each(queries.begin(), queries.end(),
+                  [&](size_t position) { queried_positions_owners.emplace_back(papers_order[position - 1]); });
+    return queried_positions_owners;
 }
