846bdcfe34cebb9e01336e562f0493095dcd85ed
Yaroslav Starostin <yistarostin@gmail.com>
2023-02-03 00:30:38 +0300

    change scorer logic (check submitted tasks after all the events handled)

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -1,6 +1,7 @@
 #include "scorer.h"
 
 #include <algorithm>
+#include <stdexcept>
 #include <unordered_map>
 #include <unordered_set>
 
@@ -13,24 +14,30 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
         sorted_happened_events;  // anyway we will need O(N logN) complexity so lets just use set for simplicity
     std::copy_if(events.begin(), events.end(), std::inserter(sorted_happened_events, sorted_happened_events.begin()),
                  [score_time](const auto& event) { return event.time <= score_time; });
-    std::unordered_map<TaskName, std::unordered_set<StudentName>> merges_per_task;
+    std::unordered_map<StudentName, std::map<TaskName, size_t>> opened_merge_requsts;
+    std::unordered_map<StudentName, std::map<TaskName, bool>> last_submit_results;
     ScoreTable currently_accepted;
     for (auto& [guy, task, time, type] : sorted_happened_events) {
         switch (type) {
             case EventType::MergeRequestClosed:
-                merges_per_task[task].erase(guy);
+                --opened_merge_requsts[guy][task];
                 break;
             case EventType::MergeRequestOpen:
-                merges_per_task[task].insert(guy);
+                ++opened_merge_requsts[guy][task];
                 break;
             case EventType::CheckFailed:
-                currently_accepted[guy].erase(task);
+                last_submit_results[guy][task] = false;
                 break;
             case EventType::CheckSuccess:
-                if (merges_per_task[task].count(guy) == 0) {
-                    currently_accepted[guy].insert(task);
-                }
+                last_submit_results[guy][task] = true;
                 break;
+        }
+    }
+    for (auto& [student, tasks_attempts] : last_submit_results) {
+        for (auto& [task, result] : tasks_attempts) {
+            if (result == true && opened_merge_requsts[student][task] == 0) {
+                currently_accepted[student].insert(task);
+            }
         }
     }
     std::erase_if(currently_accepted, [](const auto& student_results) { return student_results.second.empty(); });
