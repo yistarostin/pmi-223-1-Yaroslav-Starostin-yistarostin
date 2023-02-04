0892981a05c7d0b7a771f84b40d606428eeb0085
Yaroslav Starostin <yistarostin@gmail.com>
2023-02-04 21:19:22 +0300

    use hand-made string split instaed of rv::split, cause for some reason it doens't compile with clang++ :/

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -3,18 +3,30 @@
 #include <ranges>
 #include <vector>
 
-constexpr std::string_view UNIX_DELIMITER = "/";
-constexpr std::string_view GOTO_PREV_DIRECTORY = "..";
-constexpr std::string_view GOTO_CURR_DIRECTORY = ".";
+constexpr char UnixDelimiter = '/';
+constexpr std::string_view GotoPrevDirectory = "..";
+constexpr std::string_view GotoCurrDirectory = ".";
 
+std::vector<std::string_view> Split(std::string_view str, char delimeter){
+    std::vector<std::string_view> splitted_str;
+    auto l = str.begin();
+    for(auto r = str.begin(); r  != str.end(); ++r){
+        if(*r == delimeter){
+            splitted_str.emplace_back(std::string_view{l, r});
+            l = r+1;
+        }
+    }
+    splitted_str.emplace_back(std::string_view{l, str.end()});
+    return splitted_str;
+}
 void WalkThePath(std::string_view path, std::vector<std::string_view>& current_location) {
-    for (const auto word : std::views::split(path, UNIX_DELIMITER)) {
+    for (const auto word : Split(path, UnixDelimiter)) {
         std::string_view now = std::string_view(word.begin(), word.end());
-        if (now == GOTO_PREV_DIRECTORY) {
+        if (now == GotoPrevDirectory) {
             if (!current_location.empty()) {  // if we are not already in /
                 current_location.pop_back();
             }
-        } else if (!now.empty() && now != GOTO_CURR_DIRECTORY) {
+        } else if (!now.empty() && now != GotoCurrDirectory) {
             current_location.push_back(now);
         }
     }
@@ -26,12 +38,11 @@ std::string NormalizePath(std::string_view current_working_dir, std::string_view
     WalkThePath(local_path, current_location);
     std::string normalized_path;
     for (auto directory : current_location) {
-        normalized_path += UNIX_DELIMITER;
+        normalized_path += UnixDelimiter;
         normalized_path += directory;
     }
     if (normalized_path.empty()) {
-        normalized_path += UNIX_DELIMITER;
+        normalized_path += UnixDelimiter;
     }
     return normalized_path;
 }
-
