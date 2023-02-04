09e85b9609d3b6b03cbd304da199e109cc174942
Yaroslav Starostin <yistarostin@gmail.com>
2023-02-04 21:20:55 +0300

    lint the code

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -7,18 +7,19 @@ constexpr char UnixDelimiter = '/';
 constexpr std::string_view GotoPrevDirectory = "..";
 constexpr std::string_view GotoCurrDirectory = ".";
 
-std::vector<std::string_view> Split(std::string_view str, char delimeter){
+std::vector<std::string_view> Split(std::string_view str, char delimeter) {
     std::vector<std::string_view> splitted_str;
     auto l = str.begin();
-    for(auto r = str.begin(); r  != str.end(); ++r){
-        if(*r == delimeter){
+    for (auto r = str.begin(); r != str.end(); ++r) {
+        if (*r == delimeter) {
             splitted_str.emplace_back(std::string_view{l, r});
-            l = r+1;
+            l = r + 1;
         }
     }
     splitted_str.emplace_back(std::string_view{l, str.end()});
     return splitted_str;
 }
+
 void WalkThePath(std::string_view path, std::vector<std::string_view>& current_location) {
     for (const auto word : Split(path, UnixDelimiter)) {
         std::string_view now = std::string_view(word.begin(), word.end());
