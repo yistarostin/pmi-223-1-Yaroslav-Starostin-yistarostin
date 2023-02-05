afae033dee9be6f1d58077b156df8b257ae29386
Yaroslav Starostin <yistarostin@gmail.com>
2023-02-05 03:07:17 +0300

    run linter because I can

--- tasks/search/search.cpp
+++ tasks/search/search.cpp
@@ -6,7 +6,7 @@
 #include <unordered_set>
 
 template <typename F>
-requires std::invocable<F&, int>
+    requires std::invocable<F&, int>
 std::vector<std::string_view> Tokenize(std::string_view str, F fn) {
     std::vector<std::string_view> splitted_str;
     auto l = str.begin();
@@ -24,12 +24,12 @@ std::vector<std::string_view> Tokenize(std::string_view str, F fn) {
     return splitted_str;
 }
 
-std::unordered_map<std::string_view, size_t> GenerateIDF(const std::vector<std::string_view>& tokenized_by_lines){
-    std::unordered_map<std::string_view, size_t> IDF; // word -> IDS(word)
-    for(std::string_view line : tokenized_by_lines){
+std::unordered_map<std::string_view, size_t> GenerateIDF(const std::vector<std::string_view>& tokenized_by_lines) {
+    std::unordered_map<std::string_view, size_t> IDF;  // word -> IDS(word)
+    for (std::string_view line : tokenized_by_lines) {
         std::unordered_set<std::string_view> in_current_line;
-        for(std::string_view word : Tokenize(line, [](char c){return !std::isalpha(c);})){
-            if(in_current_line.count(word) == 0){
+        for (std::string_view word : Tokenize(line, [](char c) { return !std::isalpha(c); })) {
+            if (in_current_line.count(word) == 0) {
                 in_current_line.insert(word);
                 ++IDF[word];
             }
@@ -37,23 +37,26 @@ std::unordered_map<std::string_view, size_t> GenerateIDF(const std::vector<std::
     }
     return IDF;
 }
-std::unordered_set<std::string_view> GetInterestingLines(const std::vector<std::string_view>& tokenized_by_lines, const std::vector<std::string_view>& tokenized_query){
+
+std::unordered_set<std::string_view> GetInterestingLines(const std::vector<std::string_view>& tokenized_by_lines,
+                                                         const std::vector<std::string_view>& tokenized_query) {
     std::unordered_set<std::string_view> interesting_lines;
     const std::unordered_set<std::string_view> words_bag(tokenized_query.begin(), tokenized_query.end());
-    for(std::string_view line: tokenized_by_lines){
-        auto tokenized_line = Tokenize(line, [](char c){return !std::isalpha(c);});
-        if(std::any_of(tokenized_line.begin(), tokenized_line.end(), [&words_bag](std::string_view word){return words_bag.contains(word);})){
-            interesting_lines.insert(line); //TODO: what if 2 lines and be equal ??? 😡😡
+    for (std::string_view line : tokenized_by_lines) {
+        auto tokenized_line = Tokenize(line, [](char c) { return !std::isalpha(c); });
+        if (std::any_of(tokenized_line.begin(), tokenized_line.end(),
+                        [&words_bag](std::string_view word) { return words_bag.contains(word); })) {
+            interesting_lines.insert(line);  // TODO: what if 2 lines and be equal ??? 😡😡
         }
     }
     return interesting_lines;
 }
 
-long double GetLineTF(std::string_view line, std::string_view target_word){
+long double GetLineTF(std::string_view line, std::string_view target_word) {
     std::size_t total_words = line.size();
     std::size_t matched_words = 0;
-    for(std::string_view word : Tokenize(line, [](char c){return !std::isalpha(c);})){
-        if(target_word == word){
+    for (std::string_view word : Tokenize(line, [](char c) { return !std::isalpha(c); })) {
+        if (target_word == word) {
             ++matched_words;
         }
     }
@@ -61,9 +64,9 @@ long double GetLineTF(std::string_view line, std::string_view target_word){
 }
 
 std::vector<std::string_view> Search(std::string_view text, std::string_view query, size_t results_count) {
-    std::unordered_map<std::string_view, size_t> words_count; // word -> text.count(word)
-    const auto tokenized_query{Tokenize(query, [](char c){return !std::isalpha(c);})};
-    const auto tokenized_by_words{Tokenize(text, [](char c){return !std::isalpha(c);})};
+    std::unordered_map<std::string_view, size_t> words_count;  // word -> text.count(word)
+    const auto tokenized_query{Tokenize(query, [](char c) { return !std::isalpha(c); })};
+    const auto tokenized_by_words{Tokenize(text, [](char c) { return !std::isalpha(c); })};
     const auto tokenized_by_lines{Tokenize(text, iscntrl)};
     size_t text_length{tokenized_by_words.size()};
     const auto IDF{GenerateIDF(tokenized_by_lines)};
