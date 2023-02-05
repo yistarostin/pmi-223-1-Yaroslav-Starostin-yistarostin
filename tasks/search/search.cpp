d5ca8a702ab0e214e9550c3c1641a557c1124edd
Yaroslav Starostin <yistarostin@gmail.com>
2023-02-05 18:14:53 +0300

    handle situation if some word in query is not present in text (TF-IDF should be 0)

--- tasks/search/search.cpp
+++ tasks/search/search.cpp
@@ -96,7 +96,7 @@ std::vector<std::string_view> Search(std::string_view text, std::string_view que
         long double s = 0;
         for (std::string_view word : tokenized_query) {
             auto word_idf = static_cast<long double>(idf[word]) / static_cast<long double>(tokenized_by_lines.size());
-            auto reverse_idf = std::log(1 / word_idf);
+            auto reverse_idf = word_idf == 0 ? 0 : std::log(1 / word_idf);
             auto tf = GetLineTF(line, word);
             s += reverse_idf * tf;
         }
