c4c756fc7d1f03ba3823f91f318addac2f2a7f4d
Yaroslav Starostin <yistarostin@gmail.com>
2023-02-16 20:32:31 +0300

    solve scorer2

--- tasks/scorer2/scorer.h
+++ tasks/scorer2/scorer.h
@@ -3,6 +3,8 @@
 #include <map>
 #include <set>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using StudentName = std::string;
@@ -11,6 +13,9 @@ using TaskName = std::string;
 using ScoreTable = std::map<StudentName, std::set<TaskName>>;
 
 class Scorer {
+    std::unordered_map<StudentName, std::unordered_set<TaskName>> student_succesfull_submissions_;
+    std::unordered_map<StudentName, std::unordered_map<TaskName, size_t>> opened_merge_requests_count_;
+
 public:
     void OnCheckFailed(const StudentName& student_name, const TaskName& task_name);
     void OnCheckSuccess(const StudentName& student_name, const TaskName& task_name);
