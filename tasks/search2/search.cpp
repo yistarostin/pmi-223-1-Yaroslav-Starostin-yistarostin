f87452167cc7e683fa31f5c0c3e986ed3ef69724
Yaroslav Starostin <yistarostin@gmail.com>
2023-02-21 21:27:31 +0300

    variable names

--- tasks/search2/search.cpp
+++ tasks/search2/search.cpp
@@ -108,7 +108,7 @@ auto GetInterestingLines(const std::vector<std::vector<std::string_view>>& token
 void SearchEngine::BuildIndex(std::string_view text) {
     tokenized_by_lines_ = Tokenize(text, [](char c) { return iscntrl(c); });
     tokenized_by_words_.clear();
-    index_exists_= true;
+    index_exists_ = true;
     tokenized_by_words_.reserve(tokenized_by_lines_.size());
     for (auto line : tokenized_by_lines_) {
         tokenized_by_words_.push_back(TokenizeToWords(line));
