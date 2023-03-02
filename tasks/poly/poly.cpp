41ca37c4c103b739a0d662927d06ef282ce8c167
Yaroslav Starostin <yistarostin@gmail.com>
2023-03-02 22:13:40 +0300

    solve poly

--- tasks/poly/poly.cpp
+++ tasks/poly/poly.cpp
@@ -1,1 +1,104 @@
 #include "poly.h"
+
+#include <algorithm>
+
+Poly::Poly(std::initializer_list<std::pair<std::size_t, int32_t>> powers_with_coeffs) {
+    std::size_t maximum_power = std::max_element(powers_with_coeffs.begin(), powers_with_coeffs.end())->first;
+    coeffs_.resize(maximum_power + 1);
+    for (const auto& [power, coeff] : powers_with_coeffs) {
+        coeffs_[power] = coeff;
+    }
+    TrimZeros();
+};
+
+Poly::Poly(std::initializer_list<int32_t> coeffs_values) : coeffs_(coeffs_values.begin(), coeffs_values.end()) {
+    TrimZeros();
+}
+
+Poly& Poly::operator=(const Poly& other) {
+    coeffs_ = other.coeffs_;
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const Poly& poly) {
+    if (poly.coeffs_.empty()) {
+        return os << "y = 0";
+    }
+    os << "y =";
+    for (size_t degree = poly.coeffs_.size() - 1; degree > 0; --degree) {
+        if (poly.coeffs_[degree] == 0) {
+            continue;
+        }
+        os << " " << ((degree != poly.coeffs_.size() - 1 && poly.coeffs_[degree] > 0) ? "+ " : "")
+           << poly.coeffs_[degree] << "x^" << degree;
+    }
+    if (poly.coeffs_[0] != 0) {
+        os << " " << (poly.coeffs_[0] > 0 ? "+" : "-") << " " << std::abs(poly.coeffs_[0]);
+    }
+    return os;
+}
+
+Poly& Poly::operator+=(const Poly& other) {
+    AddWithMulitplier(other, 1);
+    TrimZeros();
+    return *this;
+}
+
+Poly& Poly::operator-=(const Poly& other) {
+    AddWithMulitplier(other, -1);
+    TrimZeros();
+    return *this;
+}
+
+void Poly::AddWithMulitplier(const Poly& other, int32_t lambda, size_t offset) {
+    if (other.coeffs_.size() + offset > coeffs_.size()) {
+        coeffs_.resize(other.coeffs_.size() + offset);
+    }
+    for (size_t degree = 0; degree < other.coeffs_.size(); ++degree) {
+        coeffs_[degree + offset] += other.coeffs_[degree] * lambda;
+    }
+}
+
+Poly Poly::operator+(const Poly& other) const {
+    Poly now{*this};
+    return now += other;
+}
+
+Poly Poly::operator-(const Poly& other) const {
+    Poly now{*this};
+    return now -= other;
+}
+
+// F*ck FFT, all my homies use O(n^2) algorithms
+Poly Poly::operator*(const Poly& other) const {
+    Poly res;
+    res.coeffs_.resize((coeffs_.size() - 1) * (other.coeffs_.size() - 1));
+    for (size_t c = 0; c < coeffs_.size(); ++c) {
+        res.AddWithMulitplier(other, coeffs_[c], c);
+    }
+    return res;
+}
+
+int64_t Poly::operator()(int32_t x_value) const {
+    int64_t poly_value = 0;
+    int64_t current_value_degree = 1;  // x_value^0 is always 1
+    for (size_t degree = 0; degree < coeffs_.size(); ++degree) {
+        poly_value += coeffs_[degree] * current_value_degree;
+        current_value_degree *= x_value;
+    }
+    return poly_value;
+}
+
+Poly Poly::operator-() const {
+    Poly reversed_sign{*this};
+    for (int32_t& coeff : reversed_sign.coeffs_) {
+        coeff *= -1;
+    }
+    return reversed_sign;
+}
+
+void Poly::TrimZeros() {
+    while (!coeffs_.empty() && coeffs_.back() == 0) {
+        coeffs_.pop_back();
+    }
+}
--- tasks/poly/poly.h
+++ tasks/poly/poly.h
@@ -1,4 +1,38 @@
 #pragma once
+#include <array>
+#include <cstdint>
+#include <map>
+#include <ostream>
+#include <utility>
+#include <vector>
 
 class Poly {
+public:
+    Poly() = default;
+
+    Poly(std::initializer_list<int32_t> coeffs);
+    Poly(std::initializer_list<std::pair<size_t, int32_t>> coeffs);
+
+    Poly(const Poly& other) = default;
+
+    Poly(Poly&& other) = default;
+
+    Poly& operator=(const Poly& other);
+
+    ~Poly() = default;
+    int64_t operator()(int32_t x_value) const;
+    bool operator==(const Poly& other) const = default;
+    bool operator!=(const Poly& other) const = default;
+    Poly operator+(const Poly& other) const;
+    Poly operator-(const Poly& other) const;
+    Poly& operator+=(const Poly& other);
+    Poly& operator-=(const Poly& other);
+    Poly operator*(const Poly& other) const;
+    Poly operator-() const;
+    friend std::ostream& operator<<(std::ostream& os, const Poly& poly);
+
+private:
+    std::vector<int32_t> coeffs_;
+    void TrimZeros();
+    void AddWithMulitplier(const Poly& other, std::int32_t delta, size_t offset = 0);
 };
