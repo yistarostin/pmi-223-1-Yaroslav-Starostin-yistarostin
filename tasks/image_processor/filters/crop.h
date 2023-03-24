3883569d8dddddebd1e9c5d130f495cfe02d9946
Yaroslav Starostin <yistarostin@gmail.com>
2023-03-24 14:19:47 +0300

    remove declarations from headers to sources

--- tasks/image_processor/filters/crop.h
+++ tasks/image_processor/filters/crop.h
@@ -6,15 +6,6 @@ class CropFilter : public Filter {
     std::size_t height_;
 
 public:
-    CropFilter(std::size_t width, std::size_t height) : width_(width), height_(height) {
-    }
-
-    void operator()(Image &image) const override {
-        auto new_height = std::min(height_, image.Height());
-        auto new_width = std::min(width_, image.Width());
-        image.colors_.erase(image.colors_.begin() + static_cast<std::ptrdiff_t>(new_height), image.colors_.end());
-        for (auto &line : image.colors_) {
-            line.erase(line.begin() + static_cast<std::ptrdiff_t>(new_width), line.end());
-        }
-    }
+    CropFilter(std::size_t width, std::size_t height);
+    void operator()(Image &image) const override;
 };
