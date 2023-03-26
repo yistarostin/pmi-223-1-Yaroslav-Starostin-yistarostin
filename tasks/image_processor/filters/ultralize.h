e4e16a1b42b178af02d6b05f59120d2e7d91d978
Yaroslav Starostin <yistarostin@gmail.com>
2023-03-26 14:14:52 +0300

    linter

--- tasks/image_processor/filters/ultralize.h
+++ tasks/image_processor/filters/ultralize.h
@@ -1,6 +1,8 @@
 #pragma once
-#include "filter.h"
 #include <cmath>
+
+#include "filter.h"
+
 class UltralizeFilter : public Filter {
 public:
     UltralizeFilter() {
@@ -11,17 +13,17 @@ public:
             for (auto &pixel : line) {
                 auto &[r, g, b] = pixel;
                 auto maximum_color = std::max(std::max(r, g), b);
-                if(maximum_color == r){
+                if (maximum_color == r) {
                     pixel = {255, 0, 0};
-                }
-                else if(maximum_color == g){
+                } else if (maximum_color == g) {
                     pixel = {0, 255, 0};
-                }
-                else{
+                } else {
                     pixel = {0, 0, 255};
                 }
             }
         }
     }
-    ~UltralizeFilter() noexcept {}
+
+    ~UltralizeFilter() noexcept {
+    }
 };
